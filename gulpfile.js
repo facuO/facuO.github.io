4db2f3c6b91bfe0dfc12c790f679b0d67f7f65b4
facuOmpre <facuompre@gmail.com>
2016-04-19 02:31:58 -0300

    header change

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,15 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
 
-gulp.task('default', function() {
-	gulp.src('/').
-	  .pipe(name(''))
-  gulp.watch('*.html', ['tasks']);
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+});
+gulp.task('default',['browserSync'], function() {
+  gulp.watch('app/*.html',browserSync.reload);
+  gulp.watch('app/scripts/*.js',browserSync.reload);
+  gulp.watch('app/styles/*.css',browserSync.reload);
 });
